f82539777d4751acda4f76c6b70000a3917394a3
Carl-Stanley <cds.beml@gmail.com>
2019-11-13 12:16:18 -0700

    Commit

--- budget/budget-router.js
+++ budget/budget-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../data/dbConfig');
 const router = express.Router();
 
-
+// Get all accounts 
 router.get('/', (req, res) => {     
     db.select('*').from('accounts')
     .then(accounts => {
@@ -13,4 +13,22 @@ router.get('/', (req, res) => {
     })
 });
 
+// get one account by id 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('accounts').where({ id })
+        .then(account => {
+            if (account[0]) {
+                res.status(200).json(account);
+            } else {
+                res.status(404).json({ message: 'invalid id' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'db problem' });
+        })
+});
+
+// Delete an account 
+// Add an account 
 module.exports = router;
