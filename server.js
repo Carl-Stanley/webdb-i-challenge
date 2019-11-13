f110bf6f559d75cb7e7d2c2b1c7d8208cbbda6ae
Carl-Stanley <cds.beml@gmail.com>
2019-11-13 12:12:06 -0700

    Commit - Finally have one get working.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const db = require('./data/dbConfig');
+
+const BudgetRouter = require('./budget/budget-router');
+
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', BudgetRouter);
+
+server.get('/', (req, res) => {
+
+    res.send('<h3>Accounts DB Default</h3>');
+ 
+});
 
 module.exports = server;
