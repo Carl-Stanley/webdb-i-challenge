d6bc42fa9cbe39d60bbcdc3c24eca2d7e17f35d9
Carl-Stanley <cds.beml@gmail.com>
2019-11-13 21:07:11 -0700

    Commit

--- budget/budget-router.js
+++ budget/budget-router.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
         res.status(200).json(accounts);
     })
     .catch(err => {
-        res.status(500).json({ message: 'problem with the database' });
+        res.status(500).json({ message: 'Error' });
     })
 });
 
@@ -21,11 +21,11 @@ router.get('/:id', (req, res) => {
             if (account[0]) {
                 res.status(200).json(account);
             } else {
-                res.status(404).json({ message: 'invalid id' });
+                res.status(404).json({ message: 'id not found' });
             }
         })
         .catch(err => {
-            res.status(500).json({ message: 'db problem' });
+            res.status(500).json({ message: 'Error' });
         })
 });
 
@@ -36,9 +36,9 @@ router.delete('/:id', async (req, res) => {
     try {
         const count = await db.del().from('accounts').where({ id });
         count ? res.status(200).json({ deleted: count })
-            : res.status(404).json({ message: 'invalid id' });
+            : res.status(404).json({ message: 'id not found' });
     } catch (err) {
-        res.status(500).json({ message: 'database error', error: err });
+        res.status(500).json({ message: 'Error', error: err });
     }
 
 
@@ -52,7 +52,7 @@ router.post('/', async (req, res) => {
         const post = await db.insert(postData).into('accounts');
         res.status(201).json(post);
     } catch (err) {
-        res.status(500).json({ message: 'db problem', error: err });
+        res.status(500).json({ message: 'Error', error: err });
     }
 
     return; 
@@ -61,7 +61,7 @@ router.post('/', async (req, res) => {
             res.status(201).json(account);
         })
         .catch(err => {
-            res.status(500).json({ message: 'db problem', error: err });
+            res.status(500).json({ message: 'Error', error: err });
         });
 
 });
@@ -76,11 +76,11 @@ router.put('/:id', (req, res) => {
             if (count) {
                 res.status(200).json({ updated: count });
             } else {
-                res.status(404).json({ message: 'invalid id' });
+                res.status(404).json({ message: 'id not found' });
             }
         })
         .catch(err => {
-            res.status(500).json({ message: 'db problem' });
+            res.status(500).json({ message: 'Error' });
         });
 
 
